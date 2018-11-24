ea53b49314687205da3f255849ae6ec855ac47e5
Tom Elmore <tom.c.elmore@informa.com>
2018-11-24 08:37:43 +0000

    Add project files.

---
+++ K10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace K10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
